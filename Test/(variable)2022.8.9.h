c3c32066f642c66e567f7ed1d04b31d0591d54eb
lamuenjjang <109653003+lamuenjjang@users.noreply.github.com>
2022-08-10 21:44:27 +0900

    C Programming

---
+++ Test/(variable)2022.8.9.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+void main()
+{
+	//자료형
+
+	//자료형 <-
+	char alphabet = 'A';
+	int box = 100;
+	float pi = 3.14;
+
+	//문자열로 저장한 100입니다.
+	printf("100\n");
+
+	//box라는 변수에 저장되어 있는 값을 출력하는 100입니다.
+	printf("%d\n", box);
+	printf("%c\n", alphabet);
+	printf("%f\n", pi);
+
+	printf("Hello, world");
+
+}
+*/
