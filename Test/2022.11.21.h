9933c7181e2cd70439a69e91da056f275be43447
lamuenjjang <109653003+lamuenjjang@users.noreply.github.com>
2022-11-25 21:48:46 +0900

    2022.11.25

---
+++ Test/2022.11.21.h
@@ -0,0 +1,44 @@
+/*
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+
+
+int main() {
+	// 연결 리스트
+
+
+	// 1. 단방향 연결 리스트
+	Node* Head = NULL;
+	Node* node1 = new Node;
+	Node* node2 = new Node;
+	Node* node3 = new Node;
+	Head = node1;
+
+	Head->next = node2;
+	Head->next->next = node3;
+	Head->next->next->next = NULL;
+	node1->data = 10;
+	node2->data = 20;
+	node3->data = 30;
+
+	cout << Head->data << endl;
+	cout << Head->next->data << endl;
+	cout << Head->next->next->data << endl;
+
+	// 2. 원형 연결 리스트
+
+	// 3. 양방향 연결 리스트
+
+	return 0;
+}*/
--- Test/C++class.cpp
+++ Test/C++class.cpp
@@ -1,42 +1,56 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <stdio.h>
 
-
-
-struct Node 
+int main() 
 {
-	int data;
-	Node* next;
-};
-
-
-
-int main() {
-	// 연결 리스트
-
-
-	// 1. 단방향 연결 리스트
-	Node* Head = NULL;
-	Node* node1 = new Node;
-	Node* node2 = new Node;
-	Node* node3 = new Node;
-	Head = node1;
+	// 삽입 정렬
+	/*
+	// 배열의 모든 요소를 앞에서부터 차례대로
+	// 이미 정렬된 배열 부분과 비교하여, 자신의 위치를 찾아
+	// 삽입함 으로써 정렬을 완성하는 알고리즘입니다.
+	int arr[5] = { 8,7,9,2,6};
 	
-	Head->next = node2;
-	Head->next->next = node3;
-	Head->next->next->next = NULL;
-	node1->data = 10;
-	node2->data = 20;
-	node3->data = 30;
-
-	cout << Head->data << endl;
-	cout << Head->next->data << endl;
-	cout << Head->next->next->data << endl;
-
-	// 2. 원형 연결 리스트
+	for (int i = 1; i < 5; i++) {
+		int key = arr[i];
+		for (int j = i - 1; j >= 0 && arr[j] > key; j--) {
+			arr[j + 1] = arr[j];
+		}
+		arr[i] = key;
+	}
 	
-	// 3. 양방향 연결 리스트
-	
+	for (int k = 0; k < 5; k++) {
+		cout << arr[k] << endl;
+	}
+	*/
+
+	// 단어 공부
+	char str[255];
+	int max = 0;
+	char ans;
+	gets_s(str);
+
+	int alphabet[26] = { 0, };
+
+	for (char* ptr = str; *ptr != '\0'; ptr++) {
+		if (*ptr >= 65 && *ptr <= 90) {
+			alphabet[*ptr - 65]++;
+		}
+		else if (*ptr >= 97 && *ptr <= 122) {
+			alphabet[*ptr - 97]++;
+		}
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] == max) {
+			ans = '?';
+		}
+		else if (alphabet[i] > max) {
+			max = alphabet[i];
+			ans = 'A' + i;
+		}
+	}
+
+	printf("%c", ans);
+
+
 	return 0;
 }
